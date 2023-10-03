b34b916ccad5a160f453ded59aa679cd509528a4
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-10-02 22:41:35 -0400

    extensions

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,3 +1,3 @@
 def test_import():
-    from ros_toggle_extension.toggle_ros_versions_extension import *
-    from ros_colcon_extension.colcon_src_extension import *
+    from ros_toggle_extension.toggle_ros_versions_extension import toggle_ros_versions 
+    from ros_colcon_extension.colcon_src_extension import run_colcon_in_src 
